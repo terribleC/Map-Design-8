d2abfff631eaee9ce2200df30449f0435cdbd6e0
Student8 <Student8@10.8.33.158>
2016-01-13 13:52:33 -0500

    ???

--- Map Design 8/src/boundaries/Boundaries.java
+++ Map Design 8/src/boundaries/Boundaries.java
@@ -2,12 +2,6 @@ package boundaries;
 
 public class Boundaries {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	//hELLO!!!
 
 }
